518e8b73889a33e3610f561dad18222224dec566
van <vannb170801@gmail.com>
2022-11-12 10:48:32 +0700

    first commit

---
+++ config/connectDB.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UsertSchema = new Schema({
+  username: String,
+  password: String
+}, {
+    collection: 'user'
+});
+
+const user = mongoose.model('user', UsertSchema)
+
+module.exports = user;
